ff2d8f6de1268f311d363ffab83a91fe774b5fb2
AdanHuamani <adanhuamanicolorado@gmail.com>
2023-07-11 10:05:47 -0500

    ejercicios

--- CONTROL DE FLUJOS/ejercicio 2.py
+++ CONTROL DE FLUJOS/ejercicio 2.py
@@ -2,11 +2,9 @@
 ## Si en la lista contiene la palabra disco
 ## Mostrar la palabra y la ubicacion de su indice positivo
 lista = []
-
 for i in range(5):
     elemento = input("Ingresa un elemento: ")
     lista.append(elemento)
-
 if "disco" in lista:
     indices = [i for i, x in enumerate(lista) if x == "disco"]
     print("La palabra 'disco' se encuentra en los siguientes índices positivos:")
